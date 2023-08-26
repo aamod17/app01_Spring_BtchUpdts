70d52bd5c2eba35e585a48d387509691b4a01c05
amod <aamodpoddar@gmail.com>
2023-08-26 19:10:19 +0530

    added Spring batch updates project

---
+++ src/com/spring/test/Test1.java
@@ -0,0 +1,48 @@
+// Batch Updates with Statement
+
+package com.spring.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Test1 {
+
+	public static void main(String[] args) throws Exception{
+		
+		Connection connection = null;
+		Statement statement = null;
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/26aug23?user=root&password=root");
+			statement = connection.createStatement();
+			statement.addBatch("insert into employee values (777, 'BBB', 2100, 'Goa')");
+			statement.addBatch("insert into employee values (888, 'KKK', 2500, 'Gujurat')");
+			statement.addBatch("update employee set esal = esal+500 where esal<6000");
+			statement.addBatch("delete from employee where eid= 333");
+			int[] rowCount = statement.executeBatch();
+			//first way
+			for(int x : rowCount) {
+				System.out.println("Row Count: "+x);
+				
+			}
+			//second way
+			/*
+			for(int x = 0; x<rowCount.length; x++) {
+				
+				System.out.println("Row Count: "+rowCount[x]);
+			}
+			*/
+			
+		} catch (ClassNotFoundException|SQLException e) {
+			e.printStackTrace();
+		}
+		statement.close();
+		connection.close();
+
+	}
+
+}
---
+++ src/com/spring/test/Test2.java
@@ -0,0 +1,63 @@
+// Batch Updates with PreparedStatement
+
+package com.spring.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Test2 {
+
+	public static void main(String[] args) throws Exception{
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/26aug23?user=root&password=root");
+			preparedStatement = connection.prepareStatement("insert into student values(?,?,?,?)");
+			
+			preparedStatement.setInt(1, 111);
+			preparedStatement.setString(2, "AAA");
+			preparedStatement.setInt(3, 13);
+			preparedStatement.setString(4, "A");
+			preparedStatement.addBatch();
+			
+			preparedStatement.setInt(1, 222);
+			preparedStatement.setString(2, "BBB");
+			preparedStatement.setInt(3, 5);
+			preparedStatement.setString(4, "C");
+			preparedStatement.addBatch();
+			
+			preparedStatement.setInt(1, 333);
+			preparedStatement.setString(2, "CCC");
+			preparedStatement.setInt(3, 11);
+			preparedStatement.setString(4, "C");
+			preparedStatement.addBatch();
+			
+			preparedStatement.setInt(1, 444);
+			preparedStatement.setString(2, "DDD");
+			preparedStatement.setInt(3, 45);
+			preparedStatement.setString(4, "D");
+			preparedStatement.addBatch();
+			
+			int[] rowCount = preparedStatement.executeBatch();
+			for(int i : rowCount) {
+				System.out.println("Row Count: "+i);
+			}
+			
+			System.out.println("table updated successfully");
+			
+			
+			
+		} catch (ClassNotFoundException|SQLException e) {
+			e.printStackTrace();
+		}
+		
+		connection.close();
+
+	}
+
+}
